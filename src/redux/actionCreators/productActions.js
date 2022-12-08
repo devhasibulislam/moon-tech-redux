314a100074d2cd3f6065be3bbb9eb3da32b5106e
hasibulislam999 <hasib143sl@gmail.com>
2022-12-08 18:04:11 +0600

    product actions redux action created init

---
+++ src/redux/actionCreators/productActions.js
@@ -0,0 +1,29 @@
+import { ADD_TO_CART, ADD_TO_WISHLIST, REMOVE_FROM_CART, REMOVE_FROM_WISHLIST } from "../actionTypes/productActionType";
+
+export const addToCart = (product) => {
+  return {
+    type: ADD_TO_CART,
+    payload: product,
+  };
+};
+
+export const removeFromCart = (product) => {
+  return {
+    type: REMOVE_FROM_CART,
+    payload: product,
+  };
+};
+
+export const addToWishlist = (product) => {
+  return {
+    type: ADD_TO_WISHLIST,
+    payload: product,
+  };
+};
+
+export const removeFromWishlist = (product) => {
+  return {
+    type: REMOVE_FROM_WISHLIST,
+    payload: product,
+  };
+};
